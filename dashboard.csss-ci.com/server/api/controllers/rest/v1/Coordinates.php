9f8a61d570d1f675d68bf7d33431d05cd7022398
garam11358 <62483993+garam11358@users.noreply.github.com>
2020-10-29 12:49:29 +0900

    Update Coordinates.php

--- dashboard.csss-ci.com/server/api/controllers/rest/v1/Coordinates.php
+++ dashboard.csss-ci.com/server/api/controllers/rest/v1/Coordinates.php
@@ -5,13 +5,25 @@ require 'header.php';
 use Restserver\Libraries\REST_Controller;
 
 require APPPATH . '/libraries/REST_Controller.php';
-
+/**
+class controls im guessing coordinates of the town hall/muni
+extends from REST_Controller
+*/
 class Coordinates extends REST_Controller {
-
+    
+    /**
+    gets methods from parent class
+    @param null
+    @return null
+    */
     public function __construct() {
         parent::__construct();
     }
-
+    /**
+    puts index of townhall into file
+    @param int $id input number
+    @return null
+    */
     public function index_put($id) {
         $is_valid_token = $this->authorization_token->validateToken();
         if (!empty($is_valid_token) AND $is_valid_token['status'] === TRUE) {
@@ -29,7 +41,11 @@ class Coordinates extends REST_Controller {
             $this->response(['status' => FALSE, 'message' => $is_valid_token['message']], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+    options for index
+    @param null
+    @return object method
+    */
     public function index_options() {
         return $this->response(NULL, REST_Controller::HTTP_OK);
     }
