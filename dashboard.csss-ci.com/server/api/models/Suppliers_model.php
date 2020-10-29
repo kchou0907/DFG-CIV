6c4a57aa268c05e9d52deafc831299c859cb962b
garam11358 <62483993+garam11358@users.noreply.github.com>
2020-10-29 13:54:15 +0900

    Update Suppliers_model.php

--- dashboard.csss-ci.com/server/api/models/Suppliers_model.php
+++ dashboard.csss-ci.com/server/api/models/Suppliers_model.php
@@ -1,16 +1,34 @@
 <?php
 
+/**
+tests to see if BASEPATH is defined
+*/
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-
+/**
+class that controls Suppliers model
+extends from CI_Model
+*/
 class Suppliers_model extends CI_Model {
-
+    
+    /**
+    private var for table
+    */
     protected $table = "suppliers";
-
+    
+    /**
+    saves data
+    @param $data seems to be a list or array
+    @return method insert()
+    */
     public function save($data) {
         return $this->db->insert($this->table, $data);
     }
-
+    /**
+    finds location of data
+    @param $where seesms to be int
+    @return method result()
+    */
     public function find($where) {
         return $this->db->select('*')
                         ->from($this->table)
@@ -20,7 +38,11 @@ class Suppliers_model extends CI_Model {
                         ->get()
                         ->result();
     }
-
+    /**
+    function to control login of user, finds user login info to cross check
+    @param $where, $orWhere
+    @return method result()
+    */
     public function login($where, $orWhere) {
         return $this->db->select('*')
                         ->from($this->table)
@@ -31,7 +53,11 @@ class Suppliers_model extends CI_Model {
                         ->get()
                         ->result();
     }
-
+    /**
+    gets all data
+    @param null
+    @return method result()
+    */
     public function getAll() {
         return $this->db->select('*')
                         ->from($this->table)
@@ -40,7 +66,11 @@ class Suppliers_model extends CI_Model {
                         ->get()
                         ->result();
     }
-
+    /**
+    function to show data to user
+    @param $nbre, $base = 0 seems to have int type
+    @return method result()
+    */
     public function show($nbre, $base = 0) {
         return $this->db->select('*')
                         ->from($this->table)
@@ -50,17 +80,29 @@ class Suppliers_model extends CI_Model {
                         ->get()
                         ->result();
     }
-
+    /**
+    updates data
+    @param $data, $where  list, array type for $data and int for $where
+    @return method update()
+    */
     public function update($data, $where) {
         return $this->db->where($where)
                         ->update($this->table, $data);
     }
-
+    /**
+    delete data from table
+    @param $where int type
+    @return method delete()
+    */
     public function delete($where) {
         return $this->db->where($where)
                         ->delete($this->table);
     }
-
+    /**
+    count data from table
+    @param $data = NULL, array or list type
+    @return method count_all()
+    */
     public function count($data = NULL) {
         return $this->db->where($data)
                         ->count_all($this->table);
