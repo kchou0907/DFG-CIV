dc6b53741e8966b0d60ed34b0b20d798d90f134a
garam11358 <62483993+garam11358@users.noreply.github.com>
2020-11-03 17:38:31 +0900

    Update Suppliers_model.php

--- dashboard.csss-ci.com/server/api/models/Suppliers_model.php
+++ dashboard.csss-ci.com/server/api/models/Suppliers_model.php
@@ -4,13 +4,23 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Suppliers_model extends CI_Model {
-
+    /**
+     * private table/query/database for suppliers
+    */
     protected $table = "suppliers";
-
+    /**
+     * Insert data within the table for suppliers
+     * @param Array $data - array that has data for datbase
+     * @return boolean that states whether job was completed
+    */
     public function save($data) {
         return $this->db->insert($this->table, $data);
     }
-
+    /**
+     * finds the row that fullfills specifications
+     * @param Array/String $where - row of specficated data
+     * @return Array that tells the row of specfied data
+    */
     public function find($where) {
         return $this->db->select('*')
                         ->from($this->table)
@@ -20,7 +30,12 @@ class Suppliers_model extends CI_Model {
                         ->get()
                         ->result();
     }
-
+    /**
+     * returns the rows of login information
+     * @param Array/String $where - location of the row of login
+     * @param Array/String $orWhere - location of row of login
+     * @return Array - Array with specfied row for information
+    */
     public function login($where, $orWhere) {
         return $this->db->select('*')
                         ->from($this->table)
@@ -31,7 +46,11 @@ class Suppliers_model extends CI_Model {
                         ->get()
                         ->result();
     }
-
+    /**
+     * gets all of the rows in query
+     *@param null
+     * @return Array - array with all the rows of query
+    */
     public function getAll() {
         return $this->db->select('*')
                         ->from($this->table)
@@ -40,7 +59,12 @@ class Suppliers_model extends CI_Model {
                         ->get()
                         ->result();
     }
-
+    /**
+     * shows a limited amount of rows from query
+     * @param int $nbre - rows to loimit results to
+     * @param int $base = 0 - rows to skip over
+     * @return Array - array with the desired number of rows specified
+    */
     public function show($nbre, $base = 0) {
         return $this->db->select('*')
                         ->from($this->table)
@@ -50,17 +74,30 @@ class Suppliers_model extends CI_Model {
                         ->get()
                         ->result();
     }
-
+    /**
+     * updates query with data at location
+     * @param Array $data - array that hold data to be updated
+     * @param Array/String $where - location of where to be updated
+     * @returns boolean - true if sucessfully updated
+    */
     public function update($data, $where) {
         return $this->db->where($where)
                         ->update($this->table, $data);
     }
-
+    /**
+     * deletes row from query
+     * @param Array/String $where - location of row 
+     * @return boolean - true if sucessfully deleted
+    */
     public function delete($where) {
         return $this->db->where($where)
                         ->delete($this->table);
     }
-
+    /**
+     * counts all the rows within query with matching data
+     * @param Array $data = null - desired data to be matched
+     * @return int - the number of rows with desired data
+    */
     public function count($data = NULL) {
         return $this->db->where($data)
                         ->count_all($this->table);
