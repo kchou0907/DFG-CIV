7ce87ca93d3ced45ff4142666e9ed72dea12cc2e
garam11358 <62483993+garam11358@users.noreply.github.com>
2020-11-12 20:21:14 +0900

    Update Coordinates.php

--- dashboard.csss-ci.com/server/api/controllers/rest/v1/Coordinates.php
+++ dashboard.csss-ci.com/server/api/controllers/rest/v1/Coordinates.php
@@ -7,11 +7,18 @@ use Restserver\Libraries\REST_Controller;
 require APPPATH . '/libraries/REST_Controller.php';
 
 class Coordinates extends REST_Controller {
-
+    /**
+    extends the parent constructer method
+    */
     public function __construct() {
         parent::__construct();
     }
-
+    /**
+     *updates the townHall_model with the townHall name and the id of the townHall 
+      If the townhall name is empty then exits the script with bad request message. 
+      If there isn't a valid token or status is false,  exits script
+     * @param int $id - id of user
+    */
     public function index_put($id) {
         $is_valid_token = $this->authorization_token->validateToken();
         if (!empty($is_valid_token) AND $is_valid_token['status'] === TRUE) {
@@ -29,7 +36,10 @@ class Coordinates extends REST_Controller {
             $this->response(['status' => FALSE, 'message' => $is_valid_token['message']], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+     * gets the response that HTTP is okay to go 
+     * @ return null - runs the script with HTTP okay
+    */
     public function index_options() {
         return $this->response(NULL, REST_Controller::HTTP_OK);
     }
