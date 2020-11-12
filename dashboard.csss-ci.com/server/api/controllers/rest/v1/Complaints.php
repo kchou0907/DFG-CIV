87c6174d2ff96ff29b19f0704c11bc53f20a3156
garam11358 <62483993+garam11358@users.noreply.github.com>
2020-11-12 20:15:02 +0900

    Update Complaints.php

--- dashboard.csss-ci.com/server/api/controllers/rest/v1/Complaints.php
+++ dashboard.csss-ci.com/server/api/controllers/rest/v1/Complaints.php
@@ -7,11 +7,19 @@ use Restserver\Libraries\REST_Controller;
 require APPPATH . '/libraries/REST_Controller.php';
 
 class Complaints extends REST_Controller {
-
+    /**
+    extends the parent constructer method
+    */
     public function __construct() {
         parent::__construct();
     }
-
+    /**
+     *sets up conditional that if there is a valid token and that its status is 'true', 
+      then creates an array if the id is not empty. If id is empty then all the rows from bank_model is placed in the array. 
+      Then runs script that HTTP is responsive. 
+      If there isn't a valid token or status is false,  exits script
+     * @param int $id = 0 - id of user
+    */
     public function index_get($id = 0) {
         $is_valid_token = $this->authorization_token->validateToken();
         if (!empty($is_valid_token) AND $is_valid_token['status'] === TRUE) {
@@ -25,7 +33,11 @@ class Complaints extends REST_Controller {
             $this->response(['status' => FALSE, 'message' => $is_valid_token['message']], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+     * saves user information within complaints_model query with household id, title of complaint, the complaint and date. 
+       if cannnot update query, exits script. If there is no valid token 
+       or the status of said token is false, exits script. 
+    */
     public function index_post() {
         $is_valid_token = $this->authorization_token->validateToken();
         if (!empty($is_valid_token) AND $is_valid_token['status'] === TRUE) {
@@ -47,7 +59,11 @@ class Complaints extends REST_Controller {
             $this->response(['status' => FALSE, 'message' => $is_valid_token['message']], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+     * updates complaint_model database with the id of the complaint and the complaint of the user 
+       if there is no id of complaint or complaint, exit script. 
+       Again if no valid_token or the status of token is false, exits script
+    */
     public function index_put() {
         $is_valid_token = $this->authorization_token->validateToken();
         if (!empty($is_valid_token) AND $is_valid_token['status'] === TRUE) {
@@ -68,7 +84,12 @@ class Complaints extends REST_Controller {
             $this->response(['status' => FALSE, 'message' => $is_valid_token['message']], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+     * gets the id of the townhall and sets up array with each household id that submitted a complaint
+       from complaint_models query
+       if valid_token is empty or its status is false, script exits
+     * @param String/Array $id = null - id of the user
+    */
     public function town_get($id = null) {
         $is_valid_token = $this->authorization_token->validateToken();
         if (!empty($is_valid_token) AND $is_valid_token['status'] === TRUE) {
@@ -93,7 +114,11 @@ class Complaints extends REST_Controller {
             $this->response(['status' => FALSE, 'message' => $is_valid_token['message']], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+     * gets the id of the association of user from the complaint_model
+       if valid_token is empty or its status is false, then script exits
+     * @param String/Array $id = null - id of the user
+    */
     public function association_get($id = null) {
         $is_valid_token = $this->authorization_token->validateToken();
         if (!empty($is_valid_token) AND $is_valid_token['status'] === TRUE) {
@@ -108,7 +133,11 @@ class Complaints extends REST_Controller {
             $this->response(['status' => FALSE, 'message' => $is_valid_token['message']], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+     * finds the household id of the user from the complaints model. 
+       If valid_token is empty or its status is false, exits script
+     * @param String/Array $id - id of the user
+    */
     public function household_get($id) {
         $is_valid_token = $this->authorization_token->validateToken();
         if (!empty($is_valid_token) AND $is_valid_token['status'] === TRUE) {
@@ -119,7 +148,10 @@ class Complaints extends REST_Controller {
             $this->response(['status' => FALSE, 'message' => $is_valid_token['message']], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+     * gets the response that HTTP is okay to go 
+     * @ return null - runs the script with HTTP okay
+    */
     public function index_options() {
         return $this->response(NULL, REST_Controller::HTTP_OK);
     }
